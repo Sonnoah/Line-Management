f870c85665cdf5acd26336827cd1fccf7c70810d
Sonnoah <Dllight1031@gmail.com>
2026-02-02 16:42:32 +0700

    path_v3

--- app/main/page.js
+++ app/main/page.js
@@ -4,29 +4,23 @@ import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loading } from "../components/loading";
 
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Loading } from "../components/loading";
+
 export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
- useEffect(() => {
-  const page = searchParams.get("page");
+  useEffect(() => {
+    const page = searchParams.get("page")|| "main";;
 
-  const allowPages = [
-    "check_in",
-    "profile",
-    "request_for_leave",
-    "main",
-  ];
+    if (!page) return; 
 
-  if (!page) return;
-
-  if (!allowPages.includes(page)) {
-    router.replace("/check_in");
-    return;
-  }
-
-  router.replace(`/${page}`);
-}, [searchParams]);
+    router.replace(`/${page}`);
+  }, [searchParams]); 
 
   return <Loading />;
 }
