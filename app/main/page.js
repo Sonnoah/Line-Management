8e17301d3d3d7a31bff5d304d06666eb4cfe7421
Sonnoah <Dllight1031@gmail.com>
2026-02-02 16:32:59 +0700

    path_v2

--- app/main/page.js
+++ app/main/page.js
@@ -8,10 +8,25 @@ export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const page = searchParams.get("page") || "main";
-    router.replace(`/${page}`);
-  }, []);
+ useEffect(() => {
+  const page = searchParams.get("page");
+
+  const allowPages = [
+    "check_in",
+    "profile",
+    "request_for_leave",
+    "main",
+  ];
+
+  if (!page) return;
+
+  if (!allowPages.includes(page)) {
+    router.replace("/check_in");
+    return;
+  }
+
+  router.replace(`/${page}`);
+}, [searchParams]);
 
   return <Loading />;
 }
