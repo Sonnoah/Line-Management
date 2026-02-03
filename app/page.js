db28a48e6a244db2a32fd3abf79669aced7c4447
Sonnoah <Dllight1031@gmail.com>
2026-02-03 17:36:21 +0700

    path

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loading } from "../app/components/loading";
 
 const ALLOW_ROUTES = ["main", "check_in", "profile", "admin"];
 
-export default function Home() {
+function RedirectLogic() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -23,3 +23,10 @@ export default function Home() {
   return <Loading />;
 }
 
+export default function Home() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <RedirectLogic />
+    </Suspense>
+  );
+}
