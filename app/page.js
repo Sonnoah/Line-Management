0908cba63b0b236cd186d76599102509b6a1112d
Sonnoah <Dllight1031@gmail.com>
2026-02-03 17:38:15 +0700

    path

--- app/page.js
+++ app/page.js
@@ -1,32 +1,33 @@
 "use client";
 
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loading } from "../app/components/loading";
 
-const ALLOW_ROUTES = ["main", "check_in", "profile", "admin"];
-
-function RedirectLogic() {
+export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const to = searchParams.get("to") || "main";
+ useEffect(() => {
+  const page = searchParams.get("page");
 
-    if (ALLOW_ROUTES.includes(to)) {
-      router.replace(`/${to}`);
-    } else {
-      router.replace("/main");
-    }
-  }, [router, searchParams]);
+  const allowPages = [
+    "check_in",
+    "profile",
+    "request_for_leave",
+    "main",
+  ];
+
+  if (!page) return;
+
+  if (!allowPages.includes(page)) {
+    router.replace("/main");
+    return;
+  }
+
+  router.replace(`/${page}`);
+}, [searchParams]);
+
 
   return <Loading />;
-}
-
-export default function Home() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <RedirectLogic />
-    </Suspense>
-  );
 }
